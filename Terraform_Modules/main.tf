af485b0e447c1571ae5fc225ef5e7e6298a15f8f
nessq-demo <36137485+nessq-demo@users.noreply.github.com>
2024-07-11 14:42:32 +0530

    Update main.tf

--- Terraform_Modules/main.tf
+++ Terraform_Modules/main.tf
@@ -2,9 +2,9 @@ module "ec2_cluster" {
     source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
 
     name    = "my-cluster"
-    ami     = "ami-0f40c8f97004632f9"
+    ami     = "ami-0f58b397bc5c1f2e8"
     instance_type          = "t2.micro"
-    subnet_id   = "subnet-e92f9cc8"
+    subnet_id   = "subnet-0e34671623e3b76d2"
 
     tags = {
     Terraform   = "true"
