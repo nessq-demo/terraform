4e03a672cab386dd08746089cef3bcf5477320ae
nessq-demo <36137485+nessq-demo@users.noreply.github.com>
2024-07-11 16:29:51 +0530

    Update variables.tf

--- Terraform Concepts Building Blocks/variables.tf
+++ Terraform Concepts Building Blocks/variables.tf
@@ -1,23 +1,23 @@
 variable "AWS_ACCESS_KEY" {
     type = string
-    default = "AKIASMSIZOF42P2VUDSZ"
+    default = "AKIAZVV2AT5FPX6FI5V6"
 }
 
 variable "AWS_SECRET_KEY" {}
 
 variable "AWS_REGION" {
-default = "us-east-2"
+default = "ap-south-1"
 }
 
 variable "Security_Group"{
     type = list
-    default = ["sg-24076", "sg-90890", "sg-456789"]
+    default = ["sg-0cdc5cc4dca19e20c"]
 }
 
 variable "AMIS" {
     type = map
     default = {
-        us-east-1 = "ami-0f40c8f97004632f9"
+        ap-south-1 = "ami-0f58b397bc5c1f2e8"
         us-east-2 = "ami-05692172625678b4e"
         us-west-2 = "ami-0352d5a37fb4f603f"
         us-west-1 = "ami-0f40c8f97004632f9"
